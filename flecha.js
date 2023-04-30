fe1820c33e41906403a6b490fd7fd812f7ac95c3
David Loaiza <davidloaiza13@gmail.com>
2023-04-30 08:10:40 -0500

    Carga de ejercicio funciones flecha

---
+++ flecha.js
@@ -0,0 +1,10 @@
+// function sumar(a, b) {
+//     return a + b;
+// }
+// console.log(sumar(5, 10));
+
+const sumar = (a, b) => a + b
+const saludar = () => 'Hola mundo'
+
+console.log(saludar());
+console.log(sumar(5, 10));
